da3a3c4626504875dd7c7ee577e81f12e3507517
Mike Butsko <butsko.7@osu.edu>
2012-04-12 11:23:08 -0400

    Block titles

--- block.tpl.php
+++ block.tpl.php
@@ -1,6 +1,12 @@
 <div class="column block block-<?php print $block->module ?><?php print (($block_zebra == 'even') ? " last" : "") ?> " id="block-<?php print $block->module . "-" . $block->delta ?>">
-	<?php if($block->subject){ ?>
+	<?php if($block->module == 'People'){ ?>
 	<h3><a href="/people"><?php print $block->subject; ?></a></h3>
+	<?php } elseif($block->module == 'Events'){ ?>
+	<h3><a href="/events"><?php print $block->subject; ?></a></h3>
+	<?php } elseif($block->module == 'Poll'){ ?>
+	<h3><a href="/poll"><?php print $block->subject; ?></a></h3>
+	<?php } elseif($block->module == 'News'){ ?>
+	<h3><a href="/news"><?php print $block->subject; ?></a></h3>
 	<?php } ?>
 	<div class="content"><?php print $block->content ?></div>
 </div>
