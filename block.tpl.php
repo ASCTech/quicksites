72b59a539a4535e37f2b4eb5d48e7bfafeba205e
Mike Butsko <butsko.7@osu.edu>
2012-04-12 15:08:30 -0400

    Title bad

--- block.tpl.php
+++ block.tpl.php
@@ -1,4 +1,5 @@
 <div class="column block block-<?php print $block->module ?><?php print (($block_zebra == 'even') ? " last" : "") ?> " id="block-<?php print $block->module . "-" . $block->delta ?>">
+  <?php if($block-subject){ ?>
 	<?php if(strpos($block->delta, 'People') !== false){ ?>
 	<h3><a href="/directory"><?php print $block->subject; ?></a></h3>
 	<?php } elseif(strpos($block->delta, 'Events') !== false){ ?>
@@ -7,8 +8,9 @@
 	<h3><a href="/poll"><?php print $block->subject; ?></a></h3>
 	<?php } elseif(strpos($block->delta, 'News') !== false){ ?>
 	<h3><a href="/news"><?php print $block->subject; ?></a></h3>
-  <?php } elseif($block->subject){ ?>
+  <?php } else { ?>
   <h3><?php print $block->subject; ?></h3> 
+  <?php }?>
   <?php }?>
 	<div class="content"><?php print $block->content ?></div>
 </div>
