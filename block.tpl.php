4d1297ff128917bd92d02b8f30d41eb4e5a0925f
Mike Butsko <butsko.7@osu.edu>
2012-04-12 11:42:51 -0400

    People url for block title.

--- block.tpl.php
+++ block.tpl.php
@@ -1,6 +1,6 @@
 <div class="column block block-<?php print $block->module ?><?php print (($block_zebra == 'even') ? " last" : "") ?> " id="block-<?php print $block->module . "-" . $block->delta ?>">
 	<?php if($block->module == 'People'){ ?>
-	<h3><a href="/people"><?php print $block->subject; ?></a></h3>
+	<h3><a href="/directory"><?php print $block->subject; ?></a></h3>
 	<?php } elseif($block->module == 'Events'){ ?>
 	<h3><a href="/events"><?php print $block->subject; ?></a></h3>
 	<?php } elseif($block->module == 'Poll'){ ?>
