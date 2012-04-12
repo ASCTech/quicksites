5497d3260e651ffe979d9259134bd26a1c4af8f0
Mike Butsko <butsko.7@osu.edu>
2012-04-12 14:19:01 -0400

    Block titles

--- block.tpl.php
+++ block.tpl.php
@@ -1,12 +1,12 @@
 <div class="column block block-<?php print $block->module ?><?php print (($block_zebra == 'even') ? " last" : "") ?> " id="block-<?php print $block->module . "-" . $block->delta ?>">
-	<?php if($block->module == 'views-People'){ ?>
+	<?php if(strpos($block->delta, 'People')){ ?>
 	<h3><a href="/directory"><?php print $block->subject; ?></a></h3>
   <?php }?>
-	<?php elseif($block->module == 'views-Events'){ ?>
+	<?php elseif(strpos($block->delta, 'Events')){ ?>
 	<h3><a href="/events"><?php print $block->subject; ?></a></h3>
-	<?php } elseif($block->module == 'poll'){ ?>
+	<?php } elseif(strpos($block->delta, 'poll')){ ?>
 	<h3><a href="/poll"><?php print $block->subject; ?></a></h3>
-	<?php } elseif($block->module == 'views-News'){ ?>
+	<?php } elseif(strpos($block->delta == 'News')){ ?>
 	<h3><a href="/news"><?php print $block->subject; ?></a></h3>
   <?php } elseif($block->subject){ ?>
   <h3><?php print $block->subject; ?></h3> 
--- menu_collapse.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    $('ul.menu > li > ul > li > ul > li').hide();
-    $('ul.menu > li.active > ul').children().show();
-});
