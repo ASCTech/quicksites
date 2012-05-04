1c5aa627c6afd4a3dca76601bcbe9979302dc565
Mike Butsko <butsko.7@osu.edu>
2012-05-04 10:02:56 -0400

    Site slogan

--- front.tpl.php
+++ front.tpl.php
@@ -9,7 +9,7 @@
 					<?php if($messages){ ?>
 						<div id="message"><?php print $messages; ?></div>
 					<?php } ?>
-					<?php print $site_slogan; ?>
+					<p><?php print $site_slogan; ?></p>
 				</div>
 				<div id="front-links" class="span-8 append-3">
 					<?php print $front_links; ?>
