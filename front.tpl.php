f1dacf3c719d594ebb4781c8dc175be9c4170045
Mike Butsko <butsko.7@osu.edu>
2012-05-04 16:14:37 -0400

    oops

--- front.tpl.php
+++ front.tpl.php
@@ -9,7 +9,7 @@
 					<?php if($messages){ ?>
 						<div id="message"><?php print $messages; ?></div>
 					<?php } ?>
-					<p><?php print $site_mission; ?></p>
+					<p><?php print $mission; ?></p>
 				</div>
 				<div id="front-links" class="span-8 append-3">
 					<?php print $front_links; ?>
