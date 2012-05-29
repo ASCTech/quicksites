6d30630e4b695a99c6499699962ec3d972c155ed
Mike Butsko <butsko.7@osu.edu>
2012-05-29 11:14:45 -0400

    That's already declared

--- template.php
+++ template.php
@@ -69,7 +69,7 @@ if (is_null(theme_get_setting('quickSites_layout'))) {
   theme_get_setting('', TRUE);
 }
 
-function phptemplate_breadcrumb($breadcrumb) {
+function quicksites_phptemplate_breadcrumb($breadcrumb) {
   if (!empty($breadcrumb)) {
     return implode(' &gt; ', $breadcrumb);
   }
